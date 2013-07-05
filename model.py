b1379d1c1099b88c8f669b71ae2a23fba8aa4d6c
OpenShift guest <vic.mortelmans@telenet.be>
2013-07-05 14:27:22 -0400

    adding datastore functions

---
+++ model.py
@@ -0,0 +1,15 @@
+from google.appengine.ext import ndb
+
+
+class Illustration(ndb.Model):
+  id = ndb.StringProperty(required=True)
+  title = ndb.StringProperty()
+  artist = ndb.StringProperty()
+  year = ndb.TextProperty()
+  location = ndb.TextProperty()
+  copyright = ndb.TextProperty()
+  caption = ndb.TextProperty()
+  filename = ndb.TextProperty()
+  fileExtension = ndb.TextProperty() 
+  url = ndb.StringProperty
+
